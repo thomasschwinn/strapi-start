1889a865b905a313584ab3568e17a5d4c4f187ea
thomasschwinn <99867367+thomasschwinn@users.noreply.github.com>
2022-09-23 18:37:52 +0200

    update 4.1.8

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '42de62bbdd5633d4cb651e4299bc34ec'),
+    secret: env("ADMIN_JWT_SECRET", "42de62bbdd5633d4cb651e4299bc34ec"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
   },
 });
